33af62925f8dc717489f54f1a0901d27bc2a2337
Rohan Sahana <71550986+rohansahana6@users.noreply.github.com>
2020-10-19 22:18:41 +0530

    Create filepath.py
    # Function adapted from https://kirr.co/0l6qmh

---
+++ Sample/Happiiness/filepath.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+ 
+ 
+ # Set resolution for the video capture
+    # Function adapted from https://kirr.co/0l6qmh
+    def change_res(self, width, height):
+        self.capture.set(3, width)
+        self.capture.set(4, height)
+
+    def get_dims(self, res='480p'):
+        width, height = self.STD_DIMENSIONS['480p']
+        if res in self.STD_DIMENSIONS:
+            width, height = self.STD_DIMENSIONS[res]
+        self.change_res(width, height)
+        self.dims = (width, height)
+        return width, height
+
+    def get_video_type(self):
+        filename, ext = os.path.splitext(self.filepath)
+        if ext in self.VIDEO_TYPE:
+          return  self.VIDEO_TYPE[ext]
+        return self.VIDEO_TYPE['avi']
